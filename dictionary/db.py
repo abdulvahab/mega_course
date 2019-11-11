c4b84f95bc677b2e6aec1212229f22d1b126aa29
Abdulvahab Kharadi <a.kharadi@hrwallingford.com>
2019-11-11 12:55:06 +0000

    added database connectivity

---
+++ dictionary/db.py
@@ -0,0 +1,21 @@
+import json
+from difflib import get_close_matches
+from psycopg2 import connect
+
+with open("data.json") as file, open("db_config.json") as config_file:
+    data = json.load(file)
+    credential = json.load(config_file)
+    conn = connect(**credential)
+    cur = conn.cursor()
+
+print(data['advantage'])
+
+# Select query
+cur.execute("select * from dictionary where expression='advantage'")
+conn.commit()
+
+result = cur.fetchall()
+meanings = result[0][1].replace('{','').replace('}','').split(',')
+
+for meaning in meanings:
+    print(meaning)
